d363c60f5f5571eb272c74e33a6862b609339a54
Kit Jonathan Te <kjonathante@gmail.com>
2019-03-11 13:30:08 -0700

    Created public variables inside HelloWorld class.

--- variables/Assets/HelloWorld.cs
+++ variables/Assets/HelloWorld.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    // These public variables will show up as fields in Unity
+    public string text = "Hello World";
+    public float points = 3f;
+    public int posX = 5;
+
     // Start is called before the first frame update
     void Start()
     {
