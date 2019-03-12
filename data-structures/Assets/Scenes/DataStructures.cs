ea3bcb96b8e059e2e6b091554fc1de2509efcf2c
Kit Jonathan Te <kjonathante@gmail.com>
2019-03-11 19:03:24 -0700

    Added codes to manipulate arrays, list and dictionary.

--- data-structures/Assets/Scenes/DataStructures.cs
+++ data-structures/Assets/Scenes/DataStructures.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class DataStructures : MonoBehaviour
 {
+    // Arrays
+    string[] test; // declaration only
+    string[] labels = new string[5]; //initial value for each element is an empty string, size 5;
+    string[] names = new string[]{ "Jane", "Jack"};
+    // string[] entities = new string[5]{"First"}; // this produces an error
+
+    // Lists
+    // List<string> animals = new List<string>();
+    List<string> animals = new List<string>(){"dogs", "cats"};
+
+    // Dictionary
+    Dictionary<string, string> weapons = new Dictionary<string, string>();
+
     // Start is called before the first frame update
     void Start()
     {
+        // Arrays
+        print( "labels[0] : " + labels[0] );
+        labels[1] = "John";
+        print( "labels[1] : " + labels[1] );
+        print( "Total Names " + names.Length);
         
+        //Lists
+        animals.Add("parrots");
+        animals.Add("monkey");
+        animals.Remove("monkey");
+        print("Total Animals " + animals.Count);
+        print("Animal 1 " + animals[0]);
+        print("Last Animal " + animals[animals.Count-1]);
+        print("List to Array length " + animals.ToArray().Length);
+
+        //Dictionary
+        weapons.Add("slot1","Machine gun");
+        print( "Weapon in slot 1 : " + weapons["slot1"]);
+        print ( weapons.ContainsKey("slot2") );
     }
 
     // Update is called once per frame
