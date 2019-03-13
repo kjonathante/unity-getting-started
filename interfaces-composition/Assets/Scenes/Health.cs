629f15c55f0ccc474b10b33dcdba6dc1eac62ab5
Kit Jonathan Te <kjonathante@gmail.com>
2019-03-12 23:21:02 -0700

    Created interfaces-composition project. Added Health class and IHealth interface.

---
+++ interfaces-composition/Assets/Scenes/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame {
+
+    public interface IHealth {
+        int health { get; set;}
+        int maxHealth { get; set;}
+        float CurrentHealthPercent();
+        void RestoreHealth();
+    }
+
+    public class Health : IHealth {
+
+        public int health { get; set;}
+        public int maxHealth { get; set;}
+
+        public Health() {
+            health = 5;
+            maxHealth = 10;
+        }
+
+        public float CurrentHealthPercent() {
+            return (health/(float)maxHealth) * 100;
+        }
+
+        public void RestoreHealth() {
+            health = maxHealth;
+        }
+    }
+}
---
+++ interfaces-composition/Assets/Scenes/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyGame;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health cc = new Health();
+        print(cc.health +"/"+ cc.maxHealth);
+        print("Health : " +  cc.CurrentHealthPercent());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
