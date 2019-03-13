532cf7dfcd95f55a0f529dbd65a804ebb5d9b3d6
Kit Jonathan Te <kjonathante@gmail.com>
2019-03-12 23:40:22 -0700

    Added Player class to demonstrate composition

--- interfaces-composition/Assets/Scenes/HelloWorld.cs
+++ interfaces-composition/Assets/Scenes/HelloWorld.cs
@@ -8,9 +8,9 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Health cc = new Health();
+        Player cc = new Player();
         print(cc.health +"/"+ cc.maxHealth);
-        print("Health : " +  cc.CurrentHealthPercent());
+        print("Health : " +  cc.CurrentHealthPercent() +"%");
     }
 
     // Update is called once per frame
