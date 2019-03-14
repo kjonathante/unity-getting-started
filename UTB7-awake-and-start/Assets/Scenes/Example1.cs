68e3a78483a1092fb477cf737a151e78e9c2396b
Kit Jonathan Te <kjonathante@gmail.com>
2019-03-14 12:17:28 -0700

    Added awake-and-start project. Added a class that demonstrates activating an object from another object.

---
+++ UTB7-awake-and-start/Assets/Scenes/Example1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+// Make sure that Cube1 is assigned this script and is inactive at the start of the game.
+
+public class Example1 : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Example1 Awake");
+    }
+
+    void Start()
+    {
+        Debug.Log("Example1 Start");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("b"))
+        {
+            print("b key was pressed");
+        }
+    }
+}
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Example1 : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
---
+++ UTB7-awake-and-start/Assets/Scenes/Example2.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Example2 : MonoBehaviour
+{
+    // GO has Example1 script assigned to it so is Cube1
+    public GameObject GO;
+
+    void Start()
+    {
+        Debug.Log("Example2 Start");
+    }
+
+    // allow Cube1 to activated just once
+    private bool activateGO = true;
+
+    void Update()
+    {
+        if (activateGO == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Debug.Log("space key was pressed");
+                GO.SetActive(true);
+                activateGO = false;
+            }
+        }
+    }
+}
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Example2 : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
